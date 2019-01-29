32b02de504381ea8707cebb886d0ae4654b603db
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-28 22:49:17 -0500

    First test case passed

--- IceCreamPricer.UnitTests/UnitTests.cs
+++ IceCreamPricer.UnitTests/UnitTests.cs
@@ -14,7 +14,11 @@ namespace IceCreamPricer.UnitTests
         Pricer pricer = new Pricer(new HolidayChecker());
         [Test]
         public void IfTempLessThan40ThrowExceptionIcecreamShopIsclosed(){
-           
+            DateTime date = new DateTime();
+            float input = 37;
+            //var actual = pricer.GetPrice(date, input);
+            //Console.WriteLine(actual);
+            Assert.Throws<System.ArgumentException>(() => pricer.GetPrice(date, input));
         }
     }
 }
